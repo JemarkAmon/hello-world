bb666a6ec0ca2667d333b57b955051fbdb0c06c3
Jemark Amon <jemarkamon@gmail.com>
2024-05-30 09:47:43 -0400

    copy my whole lab folder

---
+++ lab1/lab1b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#name =  'Thomas'
+#print(name)
+#print('name')
+#print('I have a friend named ' + name)
+
+
+name = 'Isaac'
+
+print('How old are you ' + name + '?' )
+
+
