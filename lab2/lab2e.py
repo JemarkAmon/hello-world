c6caf5b79a31930992fa9a8fd1ed2f22b965acfb
jemarkamon <jemarkamon@gmail.com>
2024-08-08 21:34:14 -0400

    lab2

---
+++ lab2/lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Jemark Amon
+#Author ID: jamon@myseneca.ca
+#DateCreated: 2024-06-05
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2/lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Jemark Amon
+#Author ID: jamon@myseneca.ca
+#DateCreated: 2024-06-05
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
